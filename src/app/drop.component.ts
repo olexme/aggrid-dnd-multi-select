9a31e308d426fde23156f0fd9316548d827472bf
olexme <olexme@gmail.com>
2020-01-30 11:58:54 +0100

    drop area improvements

--- src/app/drop.component.ts
+++ src/app/drop.component.ts
@@ -1,28 +1,49 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SkyhookDndService, DropTargetMonitor } from '@angular-skyhook/core';
 import {GridDndKey} from './config';
 @Component({
   selector: 'drop',
-  template: `<div [dropTarget]="dropTarget"><div>DropHere</div></div>`,
+  templateUrl: './drop.component.html',
   styleUrls: [ './drop.component.css' ]
 })
-export class DropComponent implements OnDestroy {
+export class DropComponent implements OnInit, OnDestroy {
+  data: {id: string, value: string, value2: string}[] = [];
+  canDrop = false;
+  isOver = false;
   dropTarget = this.dnd.dropTarget([GridDndKey, 'TheOtherDNDSource'], {
     canDrop: (monitor: DropTargetMonitor<any, any>) => {
-      const type = monitor.getItemType();
+      const itemType = monitor.getItemType();
       // const item = monitor.getItem();
-      // console.log('items to drop', item);
-      return type === GridDndKey;
+
+      // console.log('this.isOver, this.canDrop', this.isOver, this.canDrop);
+      return itemType === GridDndKey;
     },
     drop: (monitor: DropTargetMonitor<any, any>) => {
-      const item = monitor.getItem();
-      console.log('items dropped', item);
+      const items = monitor.getItem();
+      this.data.push(...items);
+      // this.isOver = false;
+      // this.canDrop = false;
+      console.log('items dropped', items);
     }
   });
+  private subscription;
+  ngOnInit() {
+  const collectedDndState = this.dropTarget.listen(m => ({
+      canDrop: m.canDrop(),
+      isOver: m.isOver(),
+    }));
 
+  this.subscription = collectedDndState
+      .subscribe((c) => {
+        this.canDrop = !c.isOver && c.canDrop;
+        this.isOver =  c.isOver && c.canDrop;
+        console.log('this.canDrop', this.canDrop, 'this.isOver', this.isOver);
+      });
+  }
   constructor(private dnd: SkyhookDndService) { }
 
   ngOnDestroy() {
     this.dropTarget.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
